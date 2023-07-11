72e06d01b931d06a6258d865607a855de2746f82
Harshad Kr. Dandage <47813538+harshadkdandage@users.noreply.github.com>
2023-07-11 12:35:45 +0530

    Add files via upload

---
+++ yolov8_sam.py
@@ -0,0 +1,104 @@
+
+"""
+Author: Harshad Dandage
+# pip install segment_anything
+# wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+# !pip install ultralytics
+"""
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+
+
+import matplotlib, sys
+import pandas as pd
+matplotlib.use('QtAgg')
+
+import matplotlib.pyplot as plt
+from ultralytics import YOLO
+import pickle
+
+#Load the image and masks
+img_path = "D:/WeAglieProject/Images/"
+image = cv2.imread(img_path+'Cat1.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+plt.axis('on')
+plt.show()
+
+#Load an object detection model
+model = YOLO('yolov8n.pt') # load an official model
+
+objects = model(image, save=True)
+for result in objects:
+  boxes = result.boxes # Boxes object for bbox outputs
+  boxes_data = boxes.data.tolist()
+# print("Prediction Scores", predict_score)
+print("Boxes data", len(boxes_data))
+
+
+# # # names: {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+
+import sys
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamPredictor
+
+
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+# device = "cuda"
+device = "/gpu:0"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+
+predictor = SamPredictor(sam)
+predictor.set_image(image)
+
+
+for boxes in boxes_data:
+    boxes = np.array(boxes[:4])
+    print("INPUT_BOX DATA", boxes)
+
+    masks, _, _ = predictor.predict(
+        point_coords=None,
+        point_labels=None,
+        box=boxes[None, :],
+        multimask_output=False,
+    )
+
+    plt.figure(figsize=(10,10))
+    plt.imshow(image)
+    show_mask(masks, plt.gca())
+    show_box(boxes, plt.gca())
+    plt.axis('off')
+    plt.savefig(img_path+'output.png')
+    plt.show()
+
+pass
